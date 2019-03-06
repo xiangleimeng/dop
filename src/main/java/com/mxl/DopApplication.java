8cad8edfd16ae0ef3795001e8c55d7b7295c8820
xianglei.meng <Shhex,123>
2019-03-06 11:56:53 +0800

    war包

--- src/main/java/com/mxl/DopApplication.java
+++ src/main/java/com/mxl/DopApplication.java
@@ -2,12 +2,18 @@ package com.mxl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DopApplication {
+public class DopApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DopApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DopApplication.class);
+	}
 }
